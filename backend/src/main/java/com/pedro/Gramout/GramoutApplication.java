cb010120aaa798e7f621af56fac677c9e91622ac
FreitasPedro <pedrospf2018@gmail.com>
2024-12-04 20:26:22 -0300

    .

---
+++ backend/src/main/java/com/pedro/Gramout/GramoutApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.Gramout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramoutApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedro/Gramout/GramoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.Gramout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
