cc1e0d27963aa45b09d073c0a09520d075483526
FreitasPedro <pedrospf2018@gmail.com>
2024-12-17 19:57:36 -0300

    css: Avaliacoes Section

--- frontend/src/pages/Estabelecimento/avaliacoes.tsx
+++ frontend/src/pages/Estabelecimento/avaliacoes.tsx
@@ -1,13 +1,81 @@
-
+import React from "react";
 
 const Avaliacoes: React.FC = () => {
-    return (
-       <div>
+  return (
+    // Avaliacoes
+    <section id="avaliacoes" className="avaliacoes">
+      <div>
         <h1>Avaliacoes</h1>
+        <div className="rate-container">
+          <div className="rate-card">
+            <div className="rate-user-basics">
+              <div className="rate-user-icon">
+                <img
+                  src="src/assets/photosExample/user-icon-1.jpg"
+                  alt="user-icon"
+                />
+              </div>
+              <span>Nome do Usuário</span>
+            </div>
+            <div className="user-rating">
+              <span>5 ★★★★★</span>
+              <span>Title</span>
+            </div>
+            <div className="rate-description">
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptates, quos, explicabo consequatur
+              </span>
+            </div>
+          </div>
 
-        
-       </div>
-    );
+          <div className="rate-card">
+            <div className="rate-user-basics">
+              <div className="rate-user-icon">
+                <img
+                  src="src/assets/photosExample/user-icon-1.jpg"
+                  alt="user-icon"
+                />
+              </div>
+              <span>Nome do Usuário</span>
+            </div>
+            <div className="user-rating">
+              <span>4 ★★★★☆</span>
+              <span>Title</span>
+            </div>
+            <div className="rate-description">
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptates, quos, explicabo consequatur
+              </span>
+            </div>
+          </div>
+
+            <div className="rate-card">
+                <div className="rate-user-basics">
+                <div className="rate-user-icon">
+                    <img
+                    src="src/assets/photosExample/user-icon-1.jpg"
+                    alt="user-icon"
+                    />
+                </div>
+                <span>Nome do Usuário</span>
+                </div>
+                <div className="user-rating">
+                <span>3 ★★★☆☆</span>
+                <span>Title</span>
+                </div>
+                <div className="rate-description">
+                <span>
+                    Lorem ipsum dolor sit amet
+                </span>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Avaliacoes;
